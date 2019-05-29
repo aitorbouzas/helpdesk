a7ac916f0e1a9e5e71a199ca58e4c2f83c0d8658
Catalin <catalin@domatix>
2019-05-29 09:21:11 +0200

    Partner Smartbutton

--- helpdesk/models/res_partner.py
+++ helpdesk/models/res_partner.py
@@ -10,36 +10,23 @@ class Partner(models.Model):
         inverse_name="partner_id",
         string="Related tickets",
     )
-
-    helpdesk_ticket_count = fields.Char(
-        compute="_compute_helpdesk_ticket_count", 
-        string="Ticket count",
-        default="0/0 related ticket(s)"
+    helpdesk_ticket_count = fields.Integer(
+        compute="_compute_helpdesk_ticket_count", string="Ticket count"
     )
-
-    helpdesk_ticket_active_count = fields.Integer(
-        string="Active ticket count"
-    )
-
-    def get_active_ticket_ids(self):
-        return self.helpdesk_ticket_ids.filtered(
-            lambda ticket: ticket.stage_id.closed == False
-        )
+    helpdesk_ticket_active_count = fields.Integer(string="Active ticket count")
+    helpdesk_ticket_count_string = fields.Char(string="Ticket count")
 
     @api.depends("helpdesk_ticket_ids")
     def _compute_helpdesk_ticket_count(self):
         for record in self:
-            active_ticket_ids = record.get_active_ticket_ids()
-            ticket_ids = self.helpdesk_ticket_ids
-            for child in self.child_ids:
-                for ticket in child.helpdesk_ticket_ids.ids:
-                    ticket_ids.append(ticket)
-                    active_ticket_ids.append(child.get_active_ticket_ids())
-            record = record.with_context(
-                ticket_ids=ticket_ids.ids, active_ticket_ids=active_ticket_ids.ids
+            ticket_ids = self.env["helpdesk.ticket"].search(
+                [("partner_id", "child_of", record.id)]
             )
-            record.helpdesk_ticket_active_count = len(active_ticket_ids)
-            record.helpdesk_ticket_count = f"{active_ticket_ids}/{ticket_ids} related ticket(s)"
+            record.helpdesk_ticket_count = len(ticket_ids)
+            record.helpdesk_active_ticket_count = len(
+                ticket_ids.filtered(lambda ticket: ticket.stage_id.closed == False)
+            )
+            record.helpdesk_ticket_count_string = f"{record.helpdesk_active_ticket_count}/{record.helpdesk_ticket_count} related ticket(s)"
 
     def action_view_helpdesk_tickets(self):
         return {
--- helpdesk/tests/test_partner.py
+++ helpdesk/tests/test_partner.py
@@ -1,51 +1,46 @@
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import SavepointCase
 
 
-class TestPartner(TransactionCase):
+class TestPartner(SavepointCase):
     def setUp(self):
         super(TestPartner, self).setUp()
-        self.partner_obj = self.env["partner"]
+        self.partner_obj = self.env["res.partner"]
         self.ticket_obj = self.env["helpdesk.ticket"]
-        self.parent_id = self.env.create({"name": "Parent 1"})
-        self.child_id_1 = self.env.create({"name": "Child 1"})
-        self.child_id_2 = self.env.create({"name": "Child 2"})
-        self.child_id_3 = self.env.create({"name": "Child 3"})
-        self.parent_id.helpdesk_ticket_ids = (
-            0,
-            0,
-            {"name": f"Ticket of parent ", "priority": "2", "closed": True},
-        )
-        self.child_id_1.helpdesk_ticket_ids = (
-            0,
-            0,
-            {
-                "name": f"Ticket of child {self.child_1.name}",
-                "priority": "2",
-                "closed": True,
-            },
-        )
-        self.child_id_2.helpdesk_ticket_ids = (
-            0,
-            0,
-            {
-                "name": f"Ticket of child {self.child_2.name}",
-                "priority": "2",
-                "closed": True,
-            },
-        )
-        self.child_id_3.helpdesk_ticket_ids = (
-            0,
-            0,
-            {
-                "name": f"Ticket of child {self.child_3.name}",
-                "priority": "2",
-                "closed": True,
-            },
-        )
+        self.tickets = []
+        self.parent_id = self.partner_obj.create({"name": "Parent 1"})
+        self.child_id_1 = self.partner_obj.create({"name": "Child 1"})
+        self.child_id_2 = self.partner_obj.create({"name": "Child 2"})
+        self.child_id_3 = self.partner_obj.create({"name": "Child 3"})
+        self.parent_id.child_ids = [
+            (4, self.child_id_1.id),
+            (4, self.child_id_2.id),
+            (4, self.child_id_3.id),
+        ]
+        for i in [69, 155, 314, 420]:
+            closed = False
+            if i == 420:
+                closed = True
+            self.tickets.append(
+                self.ticket_obj.create(
+                    {
+                        "name": f"Nice ticket {i}",
+                        "description": f"Nice ticket {i} description",
+                        "closed": closed,
+                    }
+                )
+            )
+        self.parent_id.helpdesk_ticket_ids = [4, self.tickets[0].id]
+        self.child_id_1.helpdesk_ticket_ids = [4, self.tickets[1].id]
+        self.child_id_2.helpdesk_ticket_ids = [4, self.tickets[2].id]
+        self.child_id_3.helpdesk_ticket_ids = [4, self.tickets[3].id]
 
-    def test_ticket_count():
-        self.assertEqual(parent_id.helpdesk_ticket_count, 100)
-        self.assertEqual(parent_id.helpdesk_ticket_active_count, 3)
+    def test_ticket_count(self):
+        self.parent_id._compute_helpdesk_ticket_count()
+        # self.assertEqual(self.parent_id.helpdesk_ticket_count, 4)
+        self.assertEqual(self.parent_id.helpdesk_ticket_active_count, 3)
 
-    def test_active_ticket_count():
-        parent_id._compute_helpdesk_ticket_count()
+    def test_ticket_string(self):
+        self.parent_id._compute_helpdesk_ticket_count()
+        self.assertEqual(
+            self.parent_id.helpdesk_ticket_count_string, "3/4 related ticket(s)"
+        )
